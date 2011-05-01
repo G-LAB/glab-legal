093ef30c29ef21c6e1b5f89daea67218dc9079f5
Ryan Brodkin <ryan@glabstudios.com>
2011-04-30 21:49:42 -0700

    Adding a controller to handle the transactions.

---
+++ controllers/sign.php
@@ -0,0 +1,57 @@
+<?php
+class Sign extends CI_Controller {
+	
+	function __construct ()
+	{
+		parent::__construct();
+		$this->display->setStyleHeader('logo');
+		//$this->display->setStyleMasthead('blank');
+		$this->display->setStyleMain('blank');
+		$this->display->setStyleFooter('blank');
+		
+		$this->load->helper('form');
+		$this->load->library('form_validation');
+		$this->load->model('entity');
+	}
+	
+	function _remap($str) {
+		$this->index($str);
+	}
+	
+	function index ($key=false) {
+		
+		if ($this->input->post('agree')) {
+			$this->form_validation->set_rules('signature', 'signature', 'trim|required|min_length[7]|max_length[64]');
+		}
+		
+		if ($this->form_validation->run()) {
+			
+			$signature = $this->agreement->sign(
+				$key,
+				$this->input->post('agrvid'),
+				$this->input->post('signature')
+			);
+			
+			if ($signature) $this->display->setViewBody('/legal-temp/confirmation');
+			else show_error('Could not save signature.  Please contact your account manager for assistance.');
+			
+		} else {
+			
+			$request = $this->agreement->getRequest($key);
+			
+			if (count($request)) {
+				
+				$data['agreement'] = $this->agreement->getLatest($request['agid']);
+				$data['entity_sign'] = $this->entity->get($request['eid']);
+				$data['entity_parent'] = $this->entity->get($request['peid']);
+				
+				$this->display->setViewBody('/legal-temp/form', $data);
+				
+			} else {
+				show_error('The request for signature could not be located.  Please call your account manager for assistance.');
+			}
+		}
+	}
+
+}
+?>
